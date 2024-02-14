da14b734612b38f1df3f3b98d6cf46b6c065062c
kondyan2022 <kondyan.asusnexus@gmail.com>
2024-02-14 18:30:17 +0200

    simple test version

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,69 +1,111 @@
-import Avatar from 'components/Avatar'
-import logo from 'assets/logo.svg'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 
-const randoms = [
-  [1, 2],
-  [3, 4, 5],
-  [6, 7]
-]
+const BACKEND_URL = import.meta.env.VITE_BACKEND_URL
+axios.defaults.baseURL = BACKEND_URL
+interface VideoItem {
+  _id: string
+  videoId: string
+  channelId: string
+  channelTitle: string
+  title: string
+  logo_url: string
+  views: number
+  subscribers: number
+  pubDate: string
+  updatedAt: string
+  createdAt: string
+}
 
 function App() {
+  const [videoList, setVideoList] = useState<VideoItem[]>([])
+  const [currentPage, setCurrentPage] = useState<number>(0)
+  const [totalPages, setTotalPages] = useState<number>(0)
+  const [isLoading, setIsLoading] = useState<boolean>(false)
+
+  useEffect(() => {
+    const getVideoList = async () => {
+      try {
+        setIsLoading(true)
+        const response = await axios.get('stats', {
+          params: { page: currentPage }
+        })
+        setVideoList(response.data.items)
+        setTotalPages(response.data.totalPage)
+      } catch (error) {
+        console.log(error)
+      }
+      setIsLoading(false)
+    }
+    // setCurrentPage(response.daa.totalPage)
+    getVideoList()
+  }, [currentPage])
+
   return (
-    <div className="relative overflow-hidden bg-white">
-      <div className="h-screen sm:pb-40 sm:pt-24 lg:pb-48 lg:pt-40">
-        <div className="relative mx-auto max-w-7xl px-4 sm:static sm:px-6 lg:px-8">
-          <div className="sm:max-w-lg">
-            <div className="my-4">
-              <Avatar size="large" src={logo} />
-            </div>
-            <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
-              Welcome!
-            </h1>
-            <p className="mt-4 text-xl text-gray-500">
-              This is a boilerplate build with Vite, React 18, TypeScript,
-              Vitest, Testing Library, TailwindCSS 3, Eslint and Prettier.
-            </p>
-          </div>
-          <div>
-            <div className="my-10">
-              <a
-                href="vscode://"
-                className="inline-block rounded-md border border-transparent bg-indigo-600 px-8 py-3 text-center font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-700 focus:ring-offset-2"
-              >
-                Start building for free
-              </a>
-              <div
-                aria-hidden="true"
-                className="pointer-events-none mt-10 md:mt-0 lg:absolute lg:inset-y-0 lg:mx-auto lg:w-full lg:max-w-7xl"
-              >
-                <div className="absolute sm:left-1/2 sm:top-0 sm:translate-x-8 lg:left-1/2 lg:top-1/2 lg:-translate-y-1/2 lg:translate-x-8">
-                  <div className="flex items-center space-x-6 lg:space-x-8">
-                    {randoms.map((random, number) => (
-                      <div
-                        key={`random-${random[number]}`}
-                        className="grid shrink-0 grid-cols-1 gap-y-6 lg:gap-y-8"
-                      >
-                        {random.map((number) => (
-                          <div
-                            key={`random-${number}`}
-                            className="h-64 w-44 overflow-hidden rounded-lg sm:opacity-0 lg:opacity-100"
-                          >
-                            <img
-                              src={`https://picsum.photos/600?random=${number}`}
-                              alt=""
-                              className="h-full w-full bg-indigo-100 object-cover object-center"
-                            />
-                          </div>
-                        ))}
-                      </div>
-                    ))}
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+    <div className="container">
+      <div className=" flex items-center justify-center gap-4 py-4">
+        <button
+          type="button"
+          disabled={isLoading}
+          className="rounded-md bg-orange-500 px-2 py-1 text-white"
+          onClick={() => {
+            setCurrentPage((prev) => (prev > 0 ? prev - 1 : prev))
+          }}
+        >
+          prev
+        </button>
+        {`Page: ${currentPage + 1} / ${totalPages} `}
+        <button
+          className="rounded-md bg-orange-500 px-2 py-1 text-white"
+          type="button"
+          disabled={isLoading}
+          onClick={() => {
+            setCurrentPage((prev) => (prev < totalPages - 1 ? prev + 1 : prev))
+          }}
+        >
+          next
+        </button>
       </div>
+      <table>
+        <tr>
+          <th>Logo</th>
+          <th className="w-[500px]">Title</th>
+          <th>Channel</th>
+          <th>Subscribers</th>
+          <th>Views</th>
+          <th>Pub date</th>
+        </tr>
+        {!isLoading ? (
+          videoList.map(
+            ({
+              _id,
+              logo_url,
+              title,
+              channelTitle,
+              subscribers,
+              views,
+              pubDate
+            }) => (
+              <tr key={_id}>
+                <td>
+                  <img src={logo_url} />
+                </td>
+                <td>{title}</td>
+                <td>{channelTitle}</td>
+                <td className="text-center">{subscribers}</td>
+                <td className="text-center">{views}</td>
+                <td className="text-right">{`${new Date(
+                  pubDate
+                ).toLocaleDateString()} ${new Date(
+                  pubDate
+                ).toLocaleTimeString()}`}</td>
+              </tr>
+            )
+          )
+        ) : (
+          <div>... Loading</div>
+        )}
+      </table>
     </div>
   )
 }
